1ab1d22b1373ca7cd00de5366468e5029fcdec37
Leandro Luna <Leandro.j.luna@gmail.com>
2022-09-09 09:49:56 -0300

    v2 final
    * adding api and refactoring code
    
    * removing utils from gitignore
    
    * api final setup
    
    * final v2

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const { mediaMock, actorsMock } = require('./data/mock');
+const fetch = require('node-fetch');
+
+const { moviesApi, seriesApi } = require('./data/apiRequest');
+const { apiKey } = require('./utils/key');
 
 const app = express();
 app.engine('ejs', ejsMate);
@@ -9,38 +12,30 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('home', { mediaMock });
+app.get('/', async (req, res) => {
+  const movies = await moviesApi(apiKey);
+  const series = await seriesApi(apiKey);
+  res.render('home', { movies, series });
 });
 
-function generateRandomCast() {
-  let i = 0;
-  const arr = [];
-
-  while (i < 4) {
-    const randomNumber = Math.floor(Math.random() * 7);
-    if (!arr.includes(actorsMock[randomNumber])) {
-      arr.push(actorsMock[randomNumber]);
-      i++;
-    }
-  }
-  return arr;
-}
-
-app.get('/movie/:id', (req, res) => {
-  const id = req.params.id;
-  const movie = mediaMock.find((media) => media.id == id);
-  const cast = generateRandomCast();
-  res.render('detailed/movies', { movie, cast });
+app.get('/about', async (req, res) => {
+  res.render('about');
 });
 
-app.get('/serie/:id', (req, res) => {
+app.get('/movie/:id', async (req, res) => {
   const id = req.params.id;
-  const serie = mediaMock.find((media) => media.id == id);
-  const cast = generateRandomCast();
-  res.render('detailed/series', { serie, cast });
+  const movie = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=pt-BR`).then((res) => res.json());
+  const casting = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=pt_BR`).then((res) => res.json());
+  res.render('detailed/movies', { movie, casting });
+});
+
+app.get('/serie/:id', async (req, res) => {
+  const id = req.params.id;
+  const serie = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=pt-BR`).then((res) => res.json());
+  const casting = await fetch(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=${apiKey}&language=pt_BR`).then((res) => res.json());
+  res.render('detailed/series', { serie, casting });
 });
 
 app.listen(3000, () => {
-   console.log('Listening port 3000');
+  console.log('Listening port 3000');
 });
