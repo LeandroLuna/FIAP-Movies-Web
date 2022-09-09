9299f7553f4f0bdbc69f37c6c8a10b32275eeb5e
Leandro Luna <leandro.j.luna@gmail.com>
2022-09-08 23:46:53 -0300

    api final setup

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
 const express = require('express');
 const path = require('path');
 const ejsMate = require('ejs-mate');
-const { mediaMock, actorsMock, moviesApi } = require('./data/mock');
+const fetch = require('node-fetch');
+
+const { moviesApi, seriesApi } = require('./data/apiRequest');
 const { apiKey } = require('./utils/key');
+
 const app = express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Creates a random cast for every refresh in movies/tv show.
-function generateRandomCast() {
-  let i = 0;
-  const arr = [];
-
-  while (i < 4) {
-    const randomNumber = Math.floor(Math.random() * 7);
-    if (!arr.includes(actorsMock[randomNumber])) {
-      arr.push(actorsMock[randomNumber]);
-      i++;
-    }
-  }
-  return arr;
-}
-
 app.get('/', async (req, res) => {
-  const movies = await moviesApi(apiKey); // Get Movie/TV Show from external API: https://developers.themoviedb.org/3/getting-started/introduction
-  // console.log(movies.results[0]);
-  res.render('home', { mediaMock, movies });
+  const movies = await moviesApi(apiKey);
+  const series = await seriesApi(apiKey);
+  res.render('home', { movies, series });
 });
 
 app.get('/about', async (req, res) => {
   res.render('about');
 });
 
-app.get('/movie/:id', (req, res) => {
+app.get('/movie/:id', async (req, res) => {
   const id = req.params.id;
-  const movie = mediaMock.find((media) => media.id == id);
-  const cast = generateRandomCast();
-  res.render('detailed/movies', { movie, cast });
+  const movie = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=pt-BR`).then((res) => res.json());
+  const casting = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${apiKey}&language=pt_BR`).then((res) => res.json());
+  res.render('detailed/movies', { movie, casting });
 });
 
-app.get('/serie/:id', (req, res) => {
+app.get('/serie/:id', async (req, res) => {
   const id = req.params.id;
-  const serie = mediaMock.find((media) => media.id == id);
-  const cast = generateRandomCast();
-  res.render('detailed/series', { serie, cast });
+  const serie = await fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=pt-BR`).then((res) => res.json());
+  const casting = await fetch(`https://api.themoviedb.org/3/tv/${id}/credits?api_key=${apiKey}&language=pt_BR`).then((res) => res.json());
+  console.log(serie);
+  res.render('detailed/series', { serie, casting });
 });
 
 app.listen(3000, () => {
